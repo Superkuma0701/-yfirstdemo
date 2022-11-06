33dd704008924b2e1b96881da843f95dc7ac6cca
Superkuma0701 <like60110@gmail.com>
2022-11-06 23:14:09 +0800

    新增專案

---
+++ Myfirstdemo/src/main/java/com/example/Felixdemo/MyfirstdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Felixdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstdemoApplication.class, args);
+	}
+
+}
